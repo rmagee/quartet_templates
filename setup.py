88e21de6e20df0dcaab59dc804df21eeed05401c
SerialLab <slab@serial-lab.com>
2018-10-24 16:39:04 -0600

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='quartet_templates',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     include_package_data=True,
     url='https://gitlab.com/serial-lab/quartet_templates',
